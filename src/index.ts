d972f6b4e753b62e687a26f184b94fcec882d54d
David F <ahiadf@gmail.com>
2020-11-15 13:43:29 +1100

    FizzBuzz

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+for (let i = 50; i < 500; i++) {
+  i % 3 == 0 && i % 5 == 0
+    ? console.log('Brunch')
+    : i % 3 == 0
+    ? console.log('Breakfast')
+    : i % 5 == 0
+    ? console.log('Lunch')
+    : console.log(i);
+}
